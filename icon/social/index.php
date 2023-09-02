e47d040c92ce8454af0e94a5238abdbf00873818
nguyenhongthe <github@nguyenhongthe.dev>
2023-09-02 20:49:29 +0700

    bổ sung static.

---
+++ icon/social/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//VI" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="vi" lang="vi">
+<head>
+    <title>NHTDEV - Nguyen Hong The Dot Com</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="This is a default index page for a new domain."/>
+	<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <style type="text/css">
+       .logo {
+		   margin-top: 80px;
+	   } 
+    </style>
+</head>
+
+<body>
+	<div class="container text-center">
+        <a href="https://www.nguyenhongthe.com/" title="Nguyen Hong The Dot Com" alt="Nguyen Hong The Dot Com" target="_blank">
+			<img src="https://img.nhtcdn.com/logo/www.nguyenhongthe.com/nguyenhongthedotcom-logo-160x160.png" alt="Nguyen Hong The Dot Com Logo" class="logo">
+		</a>
+		<h1><strong class="text-primary"><a href="https://img.nhtcdn.com/" title="NHTDC Images CDN" alt="NHTDC Images CDN" >NHTDC Images CDN</a></strong></h1><br />
+        		
+		<p class="desc">Default index page of websites managed by <a href="https://www.nguyenhongthe.com/" title="Nguyen Hong The Dot Com" alt="Nguyen Hong The Dot Com" target="_blank">Nguyen Hong The Dot Com</a></p>
+        
+    </div>
+</body>
+
+</html>

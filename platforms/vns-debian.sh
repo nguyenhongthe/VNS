0bb10bf9614d928d86a8f3ed72f2d10394e06710
nguyenhongthe <github@nguyenhongthe.dev>
2023-09-08 16:30:42 +0700

    Sửa ngôn ngữ

--- platforms/vns-debian.sh
+++ platforms/vns-debian.sh
@@ -125,6 +125,12 @@ else
 fi
 
 # Cài đặt ngôn ngữ
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_MESSAGES=en_US.UTF-8
+
 cat > /etc/default/locale <<EOT
 export LANGUAGE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8

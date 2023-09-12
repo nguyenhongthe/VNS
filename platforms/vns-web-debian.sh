0116896469eb8e4e802f0811fa8547ec78cf61e4
nguyenhongthe <github@nguyenhongthe.dev>
2023-09-12 14:19:58 +0700

    tối ưu code tạo web 1.0.1

--- platforms/vns-web-debian.sh
+++ platforms/vns-web-debian.sh
@@ -3,8 +3,8 @@
 # Tên tập lệnh: vns-web-debian.sh
 # Mô tả: Tạo user để chạy web tương thích với Python và Next.js thông qua proxy NGINX trên hệ điều hành Debian.
 # Tác giả: Nguyễn Hồng Thế <nguyenhongthe.net>
-# Ngày: 2023-08-25
-# Phiên bản: 1.0.0
+# Ngày: 2023-09-12
+# Phiên bản: 1.0.1
 # Giấy phép: Giấy phép MIT
 # Sử dụng: curl -sO https://vnscdn.com/platforms/vns-web-debian.sh && chmod +x vns-web-debian.sh && bash vns-web-debian.sh
 #=======================================================================================================================
@@ -25,7 +25,7 @@ adduser "$username"
 
 # Chuyển sang user vừa tạo và chạy Zsh
 echo "Chuyển sang user vừa tạo và chạy Zsh..."
-su - "$username" -c "curl -L http://install.ohmyz.sh | sh && exec zsh &"
+su - "$username" -c "curl -L http://install.ohmyz.sh | sh && exec zsh -c > /dev/null"
 echo
 
 # Tạo các thư mục cần thiết

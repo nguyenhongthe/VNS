6735765e190242ef3c926365216b8e0a33377fbd
nguyenhongthe <github@nguyenhongthe.dev>
2023-09-12 12:07:14 +0700

    cải thiện tính ổn định

--- platforms/vns-web-debian.sh
+++ platforms/vns-web-debian.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #=======================================================================================================================
 # Tên tập lệnh: vns-web-debian.sh
-# Mô tả: Tạo web cho user với Python và Next.js thông qua proxy NGINX trên hệ điều hành Debian.
-# Đường dẫn: public/vns/platforms/vns-web-debian.sh
+# Mô tả: Tạo user để chạy web tương thích với Python và Next.js thông qua proxy NGINX trên hệ điều hành Debian.
 # Tác giả: Nguyễn Hồng Thế <nguyenhongthe.net>
 # Ngày: 2023-08-25
 # Phiên bản: 1.0.0
@@ -15,29 +14,6 @@ echo "Tạo user"
 read -p "Nhập tên user: " username
 adduser $username
 
-# Thông tin người dùng và cơ sở dữ liệu
-USERNAME="'$username'_user"
-# Nhập password cho user
-echo "Nhập password cho user"
-read -p "Nhập password cho user: " password
-PASSWORD="'$password'"
-DATABASE="'$username'_db"
-
-# Đăng nhập vào PostgreSQL và thực hiện tạo người dùng và cơ sở dữ liệu
-sudo -u postgres psql << EOF
-CREATE USER $USERNAME WITH PASSWORD '$PASSWORD';
-CREATE DATABASE $DATABASE WITH OWNER $USERNAME;
-GRANT ALL PRIVILEGES ON DATABASE $DATABASE TO $USERNAME;
-GRANT CONNECT ON DATABASE $DATABASE TO $USERNAME;
-GRANT USAGE ON SCHEMA public TO $USERNAME;
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO $USERNAME;
-GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO $USERNAME;
-EOF
-
-# Thông báo hoàn thành
-echo "Người dùng $USERNAME và database $DATABASE đã được tạo với các quyền thích hợp."
-echo
-
 # Chuyển sang user vừa tạo
 echo "Chuyển sang user vừa tạo"
 su - $username
@@ -46,21 +22,28 @@ echo
 # Cài omyzsh cho user
 echo "Cài omyzsh cho user..."
 curl -L http://install.ohmyz.sh | sh
-
-# Thay đổi shell mặc định thành Zsh
-chsh -s /bin/zsh
+echo
 # Kích hoạt omyzsh cho user
 exec zsh
 
 # Tạo các thư mục cần thiết
+echo "Tạo các thư mục cần thiết..."
 mkdir -p ~/configs
+echo "Tạo thư mục ~/configs thành công."
 mkdir -p ~/logs
+echo "Tạo thư mục ~/logs thành công."
 mkdir -p ~/commands
+echo "Tạo thư mục ~/commands thành công."
 mkdir -p ~/www
+echo "Tạo thư mục ~/www thành công."
 mkdir -p ~/nextjs
+echo "Tạo thư mục ~/nextjs thành công."
 mkdir -p ~/ssl
+echo "Tạo thư mục ~/ssl thành công."
 
 # Thoát khỏi user
+echo "Thoát khỏi user..."
+
 exit
 
 echo "Cấu hình cơ bản cho user $username đã hoàn tất."
